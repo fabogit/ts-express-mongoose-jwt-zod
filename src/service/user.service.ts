446f00fe06895d98a59584309aa9f80dd28261f7
fabogit <fabo.dev.email@gmail.com>
2022-06-25 23:38:37 +0200

    #3
    start feature: session, session.controller, updated user.service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,11 +1,28 @@
 import { DocumentDefinition } from 'mongoose';
-// import { omit } from 'lodash';
+import { omit } from 'lodash';
+
 import UserModel, { UserDocument } from '../models/user.model';
 
 export async function createUser(input: DocumentDefinition<Omit<UserDocument, 'createdAt' | 'updatedAt' | 'comparePassword'>>) {
 	try {
-		return await UserModel.create(input);
+		const user = await UserModel.create(input);
+		// instead _omit might use $project : {_id: 0 ,..., password: 0}
+		return omit(user.toJSON(), 'password');
 	} catch (err: any) {
 		throw new Error(err);
 	}
+}
+
+export async function validatePassword(email: string, password: string) {
+	const user = await UserModel.findOne({email});
+
+	if(!user){
+		return false;
+	}
+	const isValid = await user.comparePassword(password);
+	if(!isValid){
+		return false;
+	}
+
+	return omit(user.toJSON(), 'password');
 }
