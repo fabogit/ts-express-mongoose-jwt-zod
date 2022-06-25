ab345b5870c498311a338b84bb5186511b8fd265
fabogit <fabo.dev.email@gmail.com>
2022-06-26 01:00:53 +0200

    #5
    POST @/sessions, fixed keys (CHANGE KEYS AFTER DEV!!!!!!)

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -4,7 +4,7 @@ import config from 'config';
 const privateKey: string = config.get('privateKey');
 const publicKey: string = config.get('publicKey');
 
-function signJwt(object: Object, options?: jwt.SignOptions | undefined){
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined){
 	return jwt.sign(object, privateKey, {
 		// before spread check if it is not undefined
 		...(options && options),
@@ -12,6 +12,19 @@ function signJwt(object: Object, options?: jwt.SignOptions | undefined){
 	});
 }
 
-function verifyJwt(){
-	//
+export function verifyJwt(token: string){
+	try {
+		const decoded = jwt.verify(token, publicKey);
+		return {
+			valid: true,
+			expired: false,
+			decoded
+		};
+	} catch (err: any) {
+		return {
+			valid: false,
+			expired: err.message === 'jwt expired',
+			decoded: null
+		};
+	}
 }
